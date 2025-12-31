33340f4f82d3ebfd38aa3b3a9ea7b03137dca889
bhagirathsinh9 <bhagirathn.encircle@gmail.com>
2025-12-31 11:05:42 +0530

    docs : prettier documentation added

--- src/controller/blog.controller.js
+++ src/controller/blog.controller.js
@@ -1,37 +1,37 @@
-let blogs = [];
-let id = 1;
+let blogs = []
+let id = 1
 
-global.blogs = blogs;
+global.blogs = blogs
 
 exports.getAllBlogs = (req, res) => {
-  return  res.render("blogs/index", { blogs });
-};
+  return res.render('blogs/index', { blogs })
+}
 
 exports.getBlogById = (req, res) => {
-  const blog = blogs.find(b => b.id === Number(req.params.id));
+  const blog = blogs.find((b) => b.id === Number(req.params.id))
 
-  if (!blog) return res.send("Blog not found");
-    res.render("blogs/show", { blog });
-};
+  if (!blog) return res.send('Blog not found')
+  res.render('blogs/show', { blog })
+}
 
 exports.createBlog = (req, res) => {
   blogs.push({
     id: id++,
     title: req.body.title,
-    content: req.body.content
-  });
-  return res.redirect("/blogs");
-};
+    content: req.body.content,
+  })
+  return res.redirect('/blogs')
+}
 
 exports.updateBlog = (req, res) => {
-  const blog = blogs.find(b => b.id === Number(req.params.id));
-  blog.title = req.body.title;
-  blog.content = req.body.content;
-   res.redirect("/blogs");
-};
+  const blog = blogs.find((b) => b.id === Number(req.params.id))
+  blog.title = req.body.title
+  blog.content = req.body.content
+  res.redirect('/blogs')
+}
 
 exports.deleteBlog = (req, res) => {
-  blogs = blogs.filter(b => b.id !== Number(req.params.id));
-  global.blogs = blogs;
-   res.redirect("/blogs");
-};
+  blogs = blogs.filter((b) => b.id !== Number(req.params.id))
+  global.blogs = blogs
+  res.redirect('/blogs')
+}
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,42 +1,37 @@
-const express = require("express");
+const express = require('express')
 const {
   createBlog,
   getAllBlogs,
   getBlogById,
   updateBlog,
-  deleteBlog
-} = require("../controller/blog.controller");
+  deleteBlog,
+} = require('../controller/blog.controller')
 
-const router = express.Router();
+const router = express.Router()
 
 /* ===== EJS PAGES ===== */
 
-// Home → blogs
-router.get("/", (req, res) => {
-  res.redirect("/blogs");
-});
+router.get('/', (req, res) => {
+  res.redirect('/blogs')
+})
 
-// List blogs
-router.get("/blogs", getAllBlogs);
+router.get('/blogs', getAllBlogs)
 
-// Create page
-router.get("/blogs/create", (req, res) => {
-  res.render("blogs/create");
-});
+router.get('/blogs/create', (req, res) => {
+  res.render('blogs/create')
+})
 
-// View single blog
-router.get("/blogs/:id", getBlogById);
+router.get('/blogs/:id', getBlogById)
 
-// Edit page
-router.get("/blogs/edit/:id", (req, res) => {
-  const blog = global.blogs.find(b => b.id === Number(req.params.id));
-  res.render("blogs/edit", { blog });
-});
+router.get('/blogs/edit/:id', (req, res) => {
+  const blog = global.blogs.find((b) => b.id === Number(req.params.id))
+  res.render('blogs/edit', { blog })
+})
 
-/* ===== FORM ACTIONS ===== */
+// Form Submit Route
 
-router.post("/blogs", createBlog);
-router.post("/blogs/update/:id", updateBlog);
-router.post("/blogs/delete/:id", deleteBlog);
+router.post('/blogs', createBlog)
+router.post('/blogs/update/:id', updateBlog)
+router.post('/blogs/delete/:id', deleteBlog)
 
-module.exports = router;
+module.exports = router
